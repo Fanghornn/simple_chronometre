6cf4cc0825ebe73c358702452803023527268704
fangornn <fanghornn@gmail.com>
2018-08-26 20:43:51 +0200

    Main background animation removed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Animated, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Sound from 'react-native-sound';
 
 import Counter from './counter';
@@ -7,38 +7,10 @@ import { COLORS } from './ui';
 
 const styles = {
   flex: 1,
+  backgroundColor: COLORS.PRIMARY,
 };
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Enable playback in silence mode
-    Sound.setCategory('Playback', true);
-
-    this.state = {
-      backgroundAnim: new Animated.Value(0),
-      animLoop: null,
-    };
-  }
-
-  componentDidMount() {
-    let value = 0;
-
-    this.state.animLoop = setInterval(() => {
-      const newValue = value === 0
-        ? 1
-        : 0;
-      value = newValue;
-
-      Animated.timing(this.state.backgroundAnim, {
-        toValue: value,
-        duration: 1000,
-        // useNativeDriver: true, // <-- Add this
-      }).start();        
-    },1500)
-  }
-
   playSound = () => {
     // Load the sound file 'whoosh.mp3' from the app bundle
     // See notes below about preloading sounds within initialization code below.
@@ -62,21 +34,10 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { backgroundAnim } = this.state;
-    const backgroundStyle = backgroundAnim.interpolate({
-      inputRange: [0, 1],
-      // outputRange: ['#11ff11', '#000000']  // 0 : 150, 0.5 : 75, 1 : 0
-      outputRange: [COLORS.PRIMARY, COLORS.PRIMARY_DARKER],
-    });
-
-    const style = Object.assign(styles, { backgroundColor: backgroundStyle }, {});
-
     return (
-      <Animated.View style={style}>
-        <ScrollView>
-          <Counter onTimerSound={this.playSound} />
-        </ScrollView>
-      </Animated.View>
+      <ScrollView style={styles}>
+        <Counter onTimerSound={this.playSound} />
+      </ScrollView>
     );
   }
 }
