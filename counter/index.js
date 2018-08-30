cc5b1f52ed53157b9bd65823df16a324da664ff7
fangornn <fanghornn@gmail.com>
2018-08-30 23:53:29 +0200

    Trigger bell sounds when countdown before cycles is finished

--- counter/index.js
+++ counter/index.js
@@ -104,7 +104,7 @@ export default class Counter extends React.Component {
           ...countDown,
           on: false,
         },
-      });
+      }, onTimerSound);
     } else {
       this.setState({
         ...this.state,
